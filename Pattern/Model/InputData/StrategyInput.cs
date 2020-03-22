3f34b6215bfa9f02adf1dcb2dcd42a44f82e1144
Denisa8 <denisa.st@onet.pl>
2020-03-22 17:41:35 +0100

    Poprawa w nazewnictwie i umieszczeniu plików

--- Pattern/Model/InputData/StrategyInput.cs
+++ Pattern/Model/InputData/StrategyInput.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Pattern.Model
 {
-    public class StrategyIn
+    public class StrategyInput:InputPattern
     {
         public InterfaceData IStrategy { get; set; } //tutaj będzie namespace i metody, które musza zamplementować inne klasy
         public ClassData Context { get; set; }
